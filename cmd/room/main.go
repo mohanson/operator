3171a1504d561c8e76a0ea5483a861885b726345
mohanson <mohanson@outlook.com>
2022-08-03 21:10:18 +0800

    Add getAesEnvelop

--- cmd/room/main.go
+++ cmd/room/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"log"
+	"math/rand"
 	"net/http"
+
+	"github.com/godump/doa"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
@@ -21,11 +26,34 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+var (
+	aesData = map[string]map[string]string{}
+)
+
+func getAesEnvelop(w http.ResponseWriter, r *http.Request) {
+	rsa := r.URL.Query().Get("rsaPublicKey")
+	if v, ok := aesData[rsa]; ok {
+		w.Write(doa.Try(json.Marshal(v)))
+		return
+	}
+	aesKey := make([]byte, 32)
+	rand.Read(aesKey)
+	aesIV := make([]byte, 16)
+	rand.Read(aesIV)
+	ret := map[string]string{
+		"AESKey": hex.EncodeToString(aesKey),
+		"AESIV":  hex.EncodeToString(aesIV),
+	}
+	aesData[rsa] = ret
+	w.Write(doa.Try(json.Marshal(ret)))
+}
+
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/getAesEnvelop", getAesEnvelop)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
