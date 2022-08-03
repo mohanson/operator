08be45cef222b13b93b1af7b7054604430f04b66
mohanson <mohanson@outlook.com>
2022-08-03 15:36:39 +0800

    Add room

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mohanson/operator
+
+go 1.18
+
+require (
+	github.com/godump/doa v0.1.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
--- src/room.rs
+++ src/room.rs
@@ -1,24 +1,40 @@
 use std::borrow::BorrowMut;
 use std::collections::HashMap;
+use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 
-fn hand(id: u32, stream: &TcpStream, client: &HashMap<u32, Arc<TcpStream>>) {
-    let mut reader = stream.try_clone().unwrap();
+fn hand(id: u32, stream: Arc<RwLock<TcpStream>>, client: &HashMap<u32, Arc<RwLock<TcpStream>>>) {
+    let mut reader = stream.read().unwrap().try_clone().unwrap();
+    while true {
+        let mut buf: Vec<u8> = vec![0; 8];
+        reader.read_exact(&mut buf).unwrap();
+        let user_id = u32::from_le_bytes(buf[0..4].try_into().unwrap());
+        let message_size = u32::from_le_bytes(buf[4..8].try_into().unwrap());
+        buf.resize(8 + message_size as usize, 0);
+        reader.read_exact(&mut buf[8..]).unwrap();
+        let message = String::from_utf8_lossy(&buf[8..]);
+
+        for (k, v) in client.iter() {
+            v.write().unwrap().write_all(&buf).unwrap()
+        }
+
+        println!("{:?} {:?} {:?} {:?}", id, user_id, message_size, message);
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listen = String::from("127.0.0.1:51958");
-    let mut client = HashMap::<u32, Arc<TcpStream>>::new();
+    let mut client = HashMap::<u32, Arc<RwLock<TcpStream>>>::new();
     let mut id = 0;
     println!("listen and server on {}", listen);
     let listener = TcpListener::bind(&listen[..])?;
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let stream = Arc::new(stream);
-        client.insert(id, stream.clone());
+        let stream_lock = Arc::new(RwLock::new(stream));
+        client.insert(id, stream_lock.clone());
         let client_me = client.clone();
-        std::thread::spawn(move || hand(id, &stream, &client_me));
+        std::thread::spawn(move || hand(id, stream_lock, &client_me));
         id += 1;
     }
 
